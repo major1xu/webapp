2142b77a7936daf30c2cdadc81ca84d9c36e64b2
Minjie Xu <Minjie.Xu@ehi.com>
2015-03-22 08:57:47 -0500

    handle url can not be found case; also change error reporting level to error

--- basic_form.php
+++ basic_form.php
@@ -5,7 +5,7 @@
  * Date: 3/21/15
  * Time: 2:53 PM
  */
-error_reporting(E_ALL);
+error_reporting(E_ERROR);
 include_once('simple_html_dom.php');
 
 ?>
@@ -18,9 +18,15 @@ Welcome to
 <?php
 echo $_GET["websiteurl"];
 
+// need to check if the url is reachable, throw 404 if we can not find it
+// also needed: define a page size limit, if too big, it could crash the server or take too long, so throw an error message
+// nice to have: add a progressive bar
 $html = file_get_html($_GET["websiteurl"]);
 
-echo $html;
+if($html == false)
+    echo "<br>Failed to get the url: ";
+else
+    echo $html;
 ?><br>
 
 Your last name is: <?php echo $_GET["lastname"]; ?>
