464299074deafa05f10fc53014e9b67968c9b2b5
Minjie Xu <Minjie.Xu@ehi.com>
2015-03-22 20:03:08 -0500

    add a table to include both source, the text for click to get summary, and summary; total 3 columns

--- basic_form.php
+++ basic_form.php
@@ -17,10 +17,12 @@ include_once('simple_html_dom.php');
 
     <script type="text/javascript">
         $(document).ready(function(){
-            $("h1").click(function(){
-                var div = document.getElementById("dom-target");
-                var myData = div.textContent;
-                alert(myData);
+            $("h3").click(function(){
+                var source_div = document.getElementById("dom-target");
+                var myData = source_div.textContent;
+                //alert(myData);
+                var destination_div = document.getElementById("dom-summary");
+                destination_div.textContent = myData;
             });
         });
     </script>
@@ -36,30 +38,41 @@ Welcome to
 <?php
 echo $_GET["websiteurl"];
 ?>
+<table>
+    <tr>
+        <td>
+            <div id="dom-target" >
+            <?php
+            // need to check if the url is reachable, throw 404 if we can not find it
+            // also needed: define a page size limit, if too big, it could crash the server or take too long, so throw an error message
+            // nice to have: add a progressive bar
+            $html = file_get_html($_GET["websiteurl"]);
 
-<div id="dom-target" >
-<?php
-// need to check if the url is reachable, throw 404 if we can not find it
-// also needed: define a page size limit, if too big, it could crash the server or take too long, so throw an error message
-// nice to have: add a progressive bar
-$html = file_get_html($_GET["websiteurl"]);
+            if($html == false) {
+                echo "<br>Failed to get the url: ";
+            }
+            else {
+                echo "<br>";
+                echo "<textarea id=\"Desc\" cols=\"45\" rows=\"30\" wrap=\"soft\" name=\"Desc\">";
+                echo $html;
+                echo "</textarea>";
+            }
+            ?>
+            </div>
 
-if($html == false) {
-    echo "<br>Failed to get the url: ";
-}
-else {
-    echo "<br>";
-    echo "<textarea id=\"Desc\" cols=\"45\" rows=\"30\" wrap=\"soft\" name=\"Desc\">";
-    echo $html;
-    echo "</textarea>";
-}
-?>
-</div>
-
+        </td>
+        <td><h3>Click to show summary.</h3></td>
+        <td>
+            <div id="dom-summary">
+                <textarea id="summary" cols="45" rows="30" wrap="soft" name="Summary">
+                </textarea>
+            </div>
+        </td>
+    </tr>
+</table>
 <?php
 echo "<br>";
 ?>
-<h1>Click here to show summary.</h1>
 
 </body>
 </html>
