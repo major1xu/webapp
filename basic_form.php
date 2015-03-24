d4f436a923bb212ae9446b2a32593ac81c604692
Minjie Xu <Minjie.Xu@ehi.com>
2015-03-23 22:12:49 -0500

    print out the map content: the html tag and the count

--- basic_form.php
+++ basic_form.php
@@ -32,34 +32,54 @@ if($html == false) {
             $("h3").click(function(){
                 var source_div = document.getElementById("dom-target");
                 var html_textarea = GetElementInsideContainer("dom-target", "Desc");
-                var destination_div = document.getElementById("dom-summary");
-
-                destination_div.textContent = html_textarea.textContent;
                 parser = new DOMParser();
                 doc = parser.parseFromString(html_textarea.textContent, "text/xml");
                 // here we do the summary stuff, also need to bind the click of tags with the dom-target (the source code)
                 // a rough idea: traverse the html document, for each tag, put it in a hash map, also create a count for
                 // each tag. At the end, show the tag/count pairs.
 
-                walkDOM(doc);
+                var hashmap = walkDOM(doc);
+                var destination_div = document.getElementById("dom-summary");
+                console.log(hashmap);
+                var string ='';
+                // print out map content
+                // https://sunfishempire.wordpress.com/2014/08/19/5-ways-to-use-a-javascript-hashmap/
+                for (var x in hashmap)
+                {
+                    string = string + x;
+                    string = string + ":";
+                    var value = hashmap[x];
+                    string = string + value;
+                    string = string + ', ';
+                }
+                destination_div.textContent = string;
             });
         });
 
         // http://stackoverflow.com/questions/8747086/most-efficient-way-to-iterate-over-all-dom-elements
         function walkDOM(main) {
             var arr = [];
+            var map = new Object();
+            // http://stackoverflow.com/questions/4246980/how-to-create-a-simple-map-using-javascript-jquery
+            function get(k) {
+                return map[k];
+            }
             var loop = function(main) {
                 do {
-                    if(main.nodeType == 1)
+                    if(main.nodeType == 1) {
+                        if(get(main.tagName) == null) // new element
+                            map[main.tagName] = 1;
+                        else  // existing element, increase count by 1, and put it back to map
+                            map[main.tagName] = get(main.tagName) + 1;
                         arr.push(main);
+                    }
                     if(main.hasChildNodes())
                         loop(main.firstChild);
                 }
                 while (main = main.nextSibling);
             }
             loop(main);
-            alert(arr.toString());
-            return arr;
+            return map;
         }
 
         // http://stackoverflow.com/questions/7171483/simple-way-to-get-element-by-id-within-a-div-tag
