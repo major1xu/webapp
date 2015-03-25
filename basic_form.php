9c81f470983eea2b5fede134be5e73b759ddc940
Minjie Xu <Minjie.Xu@ehi.com>
2015-03-24 22:08:51 -0500

    some clean up; start working on the click function for the html tag

--- basic_form.php
+++ basic_form.php
@@ -30,14 +30,13 @@ if($html == false) {
     <script type="text/javascript">
         $(document).ready(function(){
             $("h3").click(function(){
-                var source_div = document.getElementById("dom-target");
-                var html_textarea = GetElementInsideContainer("dom-target", "Desc");
+                var source_div = document.getElementById("dom-source");
+                var html_textarea = GetElementInsideContainer("dom-source", "Desc");
 
-                // ??? how do I tie the hashmap back to source_div ???
                 parser = new DOMParser();
                 doc = parser.parseFromString(html_textarea.textContent, "text/xml");
 
-                // here we do the summary stuff, also need to bind the click of tags with the dom-target (the source code)
+                // here we do the summary stuff, also need to bind the click of tags with the dom-source (the source code)
                 // a rough idea: traverse the html document, for each tag, put it in a hash map, also create a count for
                 // each tag. At the end, show the tag/count pairs.
 
@@ -48,7 +47,7 @@ if($html == false) {
                 // print out map content
                 // https://sunfishempire.wordpress.com/2014/08/19/5-ways-to-use-a-javascript-hashmap/
                 //
-                // MXU: think add a click function on each tag, upon click, we go to source_div (dom-target), and highlight
+                // MXU: think add a click function on each tag, upon click, we go to source_div (dom-source), and highlight
                 // the corresponding elements.
                 //
                 for (var x in hashmap)
@@ -58,12 +57,18 @@ if($html == false) {
                     var value = hashmap[x];
                     string = string + value;
                     string = string + ', ';
-                    
+
                     var inputElement = document.createElement('input');
                     inputElement.type = "button"
                     inputElement.value = x + ":" + value;
                     inputElement.addEventListener('click', function(){
-                        alert('I am clicked');
+                        // parsing the source_div, and highlight the corresponding tag
+                        var myNodeList = document.getElementById("dom-source").getElementsByTagName(x);
+                        alert(myNodeList.length);
+                        for (var i = 0; i < myNodeList.length; ++i) {
+                            var item = myNodeList[i];  // Calling myNodeList.item(i) isn't necessary in JavaScript
+                            item.style.color="red";
+                        }
                     });
                     document.body.appendChild(inputElement);
                 }
@@ -122,7 +127,7 @@ else {
 
 <table>
         <tr>
-            <div id="dom-target" >
+            <div id="dom-source" >
             <?php
                 echo "<br>";
                 echo "<textarea id=\"Desc\" cols=\"45\" rows=\"30\" wrap=\"soft\" name=\"Desc\">";
